653e5a4a152a19e482c4960d61ae96779e9cc349
Cara Dand <cara.dand@gmail.com>
2020-05-09 21:07:35 -0600

    Made a contact page and more

--- email-templates/project-contact.php
+++ email-templates/project-contact.php
@@ -9,7 +9,7 @@
 	$budget=$_POST["budget"];
 	
 	// Email Receiver Address
-	$receiver="info@yourdomain.com";
+	$receiver="cara.dand@gmail.com";
 	$subject="Contact form details";
 
 	$message = "
--- email-templates/subscribe-newsletter.php
+++ email-templates/subscribe-newsletter.php
@@ -4,7 +4,7 @@
 	$from =$_POST["email"];
 	
 	// Email Receiver Address
-	$receiver="info@yourdomain.com";
+	$receiver="cara.dand@gmail.com";
 	$subject="Subscribe Newsletter form details";
 
 	$message = "
